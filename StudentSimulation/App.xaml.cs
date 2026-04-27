fde7e41626869e8baa0c14b2579abb38245d0026
Ali Al Kadri <ali_kadri2016@hotmail.com>
2026-04-27 13:22:30 +0200

    Add project files.

---
+++ StudentSimulation/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace StudentSimulation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
