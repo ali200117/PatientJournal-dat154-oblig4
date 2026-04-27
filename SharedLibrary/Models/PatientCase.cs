5d0da1e3511f81e92ff628e1795b7f81759c587c
Ali Al Kadri <ali_kadri2016@hotmail.com>
2026-04-27 20:54:16 +0200

    added model classes - shared library

---
+++ SharedLibrary/Models/PatientCase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary.Models
+{
+    internal class PatientCase
+    {
+
+
+        int Id;
+        
+        string Name;
+        
+        int Age;
+        
+        string Sex;
+        
+        double Weight;
+        
+        string Diagnosis;
+        
+        bool Active;
+
+    }
+}
---
+++ SharedLibrary/Models/VitalSigns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary.Models
+{
+    internal class VitalSigns
+    {
+
+
+        int Id;
+        int PatientCaseId;
+        int BloodPressure;
+        double HeartRate;
+        int OxygenSaturation;
+        double Temperature;
+        TimeSpan TimeStamp;
+    }
+}
